83e4c9cd925334f30db55439a114832cf5bf8e9a
Ubuntu <rocknrollx18@aim.com>
2017-09-08 12:25:16 -0700

    Initial Commit before pull

--- Program.cs
+++ Program.cs
@@ -2,54 +2,47 @@
 using System.Collections.Generic;
 using System.Linq;
 
-namespace Projects
-{
-    class Program
-    {
+namespace Projects {
+    class Program {
         // Converts array to list and runs program
-        static void Main(string[] args)
-        {
-            int[] initialSequence = {-2, 8, 1};
-            List<int> sequenceList = new List<int>(initialSequence);
-            AlmostSequence(ref sequenceList);
+        static void Main (string[] args) {            
+            int[] initialSequence = {-2, 8, 1 };
+            List<int> sequenceList = new List<int> (initialSequence);
+            AlmostSequence (ref sequenceList);
         }
 
         // Checks if items in list or almost Sequence
-        private static bool AlmostSequence(ref List<int> sequence)
-        {
-            if (IsSequence(ref sequence) == true)
-            {
+        private static bool AlmostSequence (ref List<int> sequence, int itemAtindex) {
+
+            if (IsSequence (ref sequence) == true) {
                 return true;
             } else {
-                RemoveItem(ref sequence);
+                
             }
             return true;
         }
 
         // Compares each item in the list. If b > a and returns false if b < a 
-        private static bool IsSequence(ref List<int> sequence)
-        {
-             for (int i = 0; i < sequence.Count - 1; i++)
-            {
-                if (sequence[i+1] > sequence[i])
-                {
+        private static bool IsSequence (ref List<int> sequence) {
+            int itemAtIndex = new int();
+            for (int i = 0; i < sequence.Count - 1; i++) {
+                if (sequence[i + 1] > sequence[i]) {
                     continue;
                 } else {
-                    
-                    
+
+                    itemAtIndex = sequence[i + 1];
+                    RemoveItem (ref sequence, itemAtIndex);
                     return false;
-                }                
+                }
             }
             return true;
         }
 
-        private static List<int> RemoveItem(ref List<int> sequence)
-        {
-            
+        private static List<int> RemoveItem (ref List<int> sequence, int itemAtIndex) {
+            return sequence;
         }
     }
 }
-
 
 /* check if list is strictly sequence
    if it is strictly sequence return true
