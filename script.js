32c2d62ea6b6b6e9c4926cb841f42c01595e1aff
Jason Mattis <rocknrollx18@aim.com>
2018-06-11 23:05:06 -0700

    Implement player ability to select a score card cell, which finalizes the players choice in points for that particular cell. Also updated a few function names for clairty.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 "use strict";
-let tableOne, tableTwo, sum;
+let tableOne, tableTwo, sum, cellValue;
 let die = [1, 1, 1, 1, 1];
 let diceImage = document.querySelectorAll(".dice");
 let tdBox = document.querySelector(".td-box");
@@ -14,19 +14,19 @@ document.querySelector("#roll-dice").addEventListener("click", function() {
   }
   dieLoop(die);
   updateTableOneVariables(tableOne);
-  updateTableData();
+  updateScore();
 });
 
 function generateRandNum(arry) {
   for (let i = 0; i < 5; i++) {
-    if (hasClass(i, "unselected") === true) {
+    let element = document.getElementById("dice-" + i);
+    if (hasClass(element, "unselected") === true) {
       die[i] = Math.floor(Math.random() * 6) + 1;
     }
   }
 }
 
-function hasClass(i, cls) {
-  let element = document.getElementById("dice-" + i);
+function hasClass(element, cls) {
   return (" " + element.className + " ").indexOf(" " + cls + " ") > -1;
 }
 
@@ -38,9 +38,9 @@ function hasClass(i, cls) {
 function dieLoop(die) {
   let fullHouseArry = [];
   for (let i = 0; i < die.length; i++) {
-    let j = i + 1;// Insures that it will not compare 'i' with itself.
+    let j = i + 1; // Insures that it will not compare 'i' with itself.
     let count = 1; // Set to 1 to include currently select die.
-    for (j; j < die.length; j++){
+    for (j; j < die.length; j++) {
       if (i === die.length) {
         break; // stop loop once at the end of the array.
       }
@@ -98,26 +98,26 @@ function checkForSmlStrt(die) {
   temp.sort();
   let newArray = removeDuplicate(temp);
   if (arraysEqual(newArray, strtOne) === true ||
-  arraysEqual(newArray, strtTwo) === true ||
-  arraysEqual(newArray, strtThree) === true) {
+    arraysEqual(newArray, strtTwo) === true ||
+    arraysEqual(newArray, strtThree) === true) {
     tableTwo[3] = strtScore;
   }
 }
 
 function removeDuplicate(temp) {
   let uniqueArray = [];
-    for(let i = 0; i < temp.length; i++){
-      if(uniqueArray.indexOf(temp[i]) == -1){
-          uniqueArray.push(temp[i]);
-      }
+  for (let i = 0; i < temp.length; i++) {
+    if (uniqueArray.indexOf(temp[i]) == -1) {
+      uniqueArray.push(temp[i]);
     }
-    removeAtIndex(uniqueArray);
-    return uniqueArray;
+  }
+  removeAtIndex(uniqueArray);
+  return uniqueArray;
 }
 
 function removeAtIndex(uniqueArray) {
-  let testCaseOne = [ 1, 2, 3, 4, 6 ];
-  let testCaseTwo = [ 1, 3, 4, 5, 6 ];
+  let testCaseOne = [1, 2, 3, 4, 6];
+  let testCaseTwo = [1, 3, 4, 5, 6];
   let testCaseThree = [2, 3, 4, 5, 6];
   if (arraysEqual(uniqueArray, testCaseOne) === true) {
     uniqueArray.splice(4, 1);
@@ -131,19 +131,19 @@ function removeAtIndex(uniqueArray) {
 
 function checkforLrgStrt(die) {
   let lrgStraightOne = [1, 2, 3, 4, 5];
-  let lrgStraightTwo = [2, 3 ,4, 5, 6];
+  let lrgStraightTwo = [2, 3, 4, 5, 6];
   let lrgStrtScore = 40
   if (arraysEqual(die, lrgStraightOne) === true ||
-  arraysEqual(die, lrgStraightTwo) === true) {
+    arraysEqual(die, lrgStraightTwo) === true) {
     tableTwo[4] = lrgStrtScore;
   }
 }
 
-function arraysEqual (arrOne, arrTwo) {
+function arraysEqual(arrOne, arrTwo) {
   if (arrOne.length !== arrTwo.length) {
     return false;
   }
-  for(let i = 0; i < arrOne.length; i++) {
+  for (let i = 0; i < arrOne.length; i++) {
     if (arrOne[i] !== arrTwo[i]) {
       return false;
     }
@@ -159,60 +159,97 @@ function updateTableOneVariables(tableOne) {
   for (let i = 0; i < die.length; i++) {
     if (die[i] === 1) {
       tableOne[0] += 1;
-    }else if (die[i] === 2) {
+    } else if (die[i] === 2) {
       tableOne[1] += 2;
-    }else if (die[i] === 3) {
+    } else if (die[i] === 3) {
       tableOne[2] += 3;
-    }else if (die[i] === 4) {
+    } else if (die[i] === 4) {
       tableOne[3] += 4;
-    }else if (die[i] === 5) {
+    } else if (die[i] === 5) {
       tableOne[4] += 5;
-    }else if (die[i] === 6) {
+    } else if (die[i] === 6) {
       tableOne[5] += 6;
     }
   }
 }
 
-function updateTableData() {
-  document.getElementById("td-ones").innerHTML = tableOne[0];
-  document.getElementById("td-twos").innerHTML = tableOne[1];
-  document.getElementById("td-threes").innerHTML = tableOne[2];
-  document.getElementById("td-fours").innerHTML = tableOne[3];
-  document.getElementById("td-fives").innerHTML = tableOne[4];
-  document.getElementById("td-sixes").innerHTML = tableOne[5];
-
-  document.getElementById("td-three-of-a-kind").innerHTML = tableTwo[0];
-  document.getElementById("td-four-of-a-kind").innerHTML = tableTwo[1];
-  document.getElementById("td-full-house").innerHTML = tableTwo[2];
-  document.getElementById("td-sml-straight").innerHTML = tableTwo[3];
-  document.getElementById("td-lrg-straight").innerHTML = tableTwo[4];
-  document.getElementById("td-chance").innerHTML = tableTwo[5];
-  document.getElementById("td-yahtzee").innerHTML = tableTwo[6];
-
-  // TODO: Needs to add rows 1-6 when every row has a value.
-  // This value will get displayed once this happens
-  document.getElementById("td-sum").innerHTML = tableOne.reduce(add);
-}
-
-function selectDie() {
-  for (let i = 0; i < diceImage.length; i++) {
-    diceImage[i].addEventListener("click", function() {
-      if (hasClass(i, "unselected") === true) {
-        this.setAttribute("style", "border: 3px solid red");
-        this.classList.remove("unselected");
-        this.classList.add("selected");
-      } else {
-        this.setAttribute("style", "border: none");
-        this.classList.remove("selected");
-        this.classList.add("unselected");
-      }
-    });
+function updateScore() {
+  let cellOne = document.getElementById("td-ones");
+  let cellTwo = document.getElementById("td-twos");
+  let cellThree = document.getElementById("td-threes");
+  let cellFour = document.getElementById("td-fours");
+  let cellFive = document.getElementById("td-fives");
+  let cellSix = document.getElementById("td-sixes");
+  if (hasClass(cellOne, "unselected") === true) {
+    document.getElementById("td-ones").innerHTML = tableOne[0];
+  } else {
+    tableOne[0] = cellValue;
   }
-}
+  if (hasClass(cellTwo, "unselected") === true) {
+    document.getElementById("td-twos").innerHTML = tableOne[1];
+  } else {
+    tableOne[1] = cellValue;
+  }
+  if (hasClass(cellThree, "unselected") === true) {
+    document.getElementById("td-threes").innerHTML = tableOne[2];
+  } else {
+    tableOne[2] = cellValue;
+  }
+  if (hasClass(cellFour, "unselected") === true) {
+    document.getElementById("td-fours").innerHTML = tableOne[3];
+  } else {
+    tableOne[3] = cellValue;
+  }
+  if (hasClass(cellFive, "unselected") === true) {
+    document.getElementById("td-fives").innerHTML = tableOne[4];
+  } else {
+    tableOne[4] = cellValue;
+  }
+  if (hasClass(cellSix, "unselected") === true) {
+    document.getElementById("td-sixes").innerHTML = tableOne[5];
+  } else {
+    tableOne[5] = cellValue;
+  }
 
-/* Called inside HTML element */
-function tableBoxClicked(clickedCell) {
-  let cellNumber = clickedCell;
-}
+    updateTableTwoData();
+  }
 
-selectDie();
+  function updateTableTwoData() {
+    document.getElementById("td-three-of-a-kind").innerHTML = tableTwo[0];
+    document.getElementById("td-four-of-a-kind").innerHTML = tableTwo[1];
+    document.getElementById("td-full-house").innerHTML = tableTwo[2];
+    document.getElementById("td-sml-straight").innerHTML = tableTwo[3];
+    document.getElementById("td-lrg-straight").innerHTML = tableTwo[4];
+    document.getElementById("td-chance").innerHTML = tableTwo[5];
+    document.getElementById("td-yahtzee").innerHTML = tableTwo[6];
+  }
+
+  function selectDie() {
+    for (let i = 0; i < diceImage.length; i++) {
+      let element = document.getElementById("dice-" + i);
+      diceImage[i].addEventListener("click", function() {
+        if (hasClass(element, "unselected") === true) {
+          this.setAttribute("style", "border: 3px solid red");
+          this.classList.remove("unselected");
+          this.classList.add("selected");
+        } else {
+          this.setAttribute("style", "border: none");
+          this.classList.remove("selected");
+          this.classList.add("unselected");
+        }
+      });
+    }
+  }
+
+  /* Called inside HTML element */
+  function getCellValue(clickedCell, element) {
+    cellValue = clickedCell;
+    updateClassList(element);
+  }
+
+  function updateClassList(element) {
+    element.classList.remove("unselected");
+    element.classList.add("selected");
+  }
+
+  selectDie();
